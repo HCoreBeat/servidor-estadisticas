038d996a00df8ead34ae55c940f518edffc90373
HCoreBeat <alederibia@gmail.com>
2025-03-07 11:52:39 -0800

    udapte

--- index.js
+++ index.js
@@ -79,5 +79,5 @@ app.delete("/limpiar-estadisticas", (req, res) => {
 });
 
 // Puerto de escucha
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => console.log(`Servidor corriendo en el puerto ${PORT}`));
